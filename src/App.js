11659421616a9d780ad4ec8bc292e8f8fde8aebe
EconClass <bizmail327@gmail.com>
2019-10-09 16:25:21 -0700

    [add] Weather Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Weather from './components/Weather'
 import './App.css';
 
+function getWeather(loc) {
+  const key = process.env.REACT_APP_API_KEY
+  const base = "https://api.openweathermap.org/data/2.5/weather"
+  const url = `${base}?q=${loc}&units=imperial&appid=${key}`
+
+  let body = null;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => { body = data })
+    .catch(err => console.log(err.message))
+
+  return body
+}
+
+
 function App() {
+  const [location, setLocation] = useState('');
+  const [weatherInfo, setWeatherInfo] = useState(getWeather('London,uk'));
+  const { name } = weatherInfo;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Weather name={name} />
+        <h1>{location}</h1>
+        <form onSubmit={e => {
+          setWeatherInfo(getWeather(location))
+          console.log(weatherInfo)
+        }}>
+          <input
+            value={location}
+            onChange={e => {
+              setLocation(e.target.value)
+            }}
+            type="text"
+            placeholder="Enter the name of a city."
+          />
+          <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
---
+++ src/components/Weather/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Weather = ({ name }) => {
+  return <h1>{name}</h1>
+};
+
+export default Weather;
